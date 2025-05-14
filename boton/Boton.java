11f5fb8a7ab8553b9564d46fcaf85703496f4e47
Mateo <mateohuertas21@gmail.com>
2025-05-14 17:49:06 +0200

    dos botones

--- boton/Boton.java
+++ boton/Boton.java
@@ -1,6 +1,7 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import java.awt.FlowLayout;
+import java.awt.Color;
 
 public class Boton {
     public static void main(String[] args) {
@@ -8,9 +9,15 @@ public class Boton {
         frame.setSize(500, 500);
 
         frame.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 80));// centrar el boton
+
+        JButton boton1 = new JButton("Click");
+        frame.add(boton1);
+        boton1.setForeground(Color.BLUE);
+
+        JButton boton2 = new JButton("Clic aquí");
+        boton2.setBackground(Color.RED);
+        frame.add(boton2);
         
-        JButton boton = new JButton("Click");
-        frame.add(boton);
         frame.setVisible(true);
     }
 }
